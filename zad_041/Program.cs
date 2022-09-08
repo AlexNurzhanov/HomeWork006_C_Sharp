9c29757ad13a4348cac3633cc492613131a6c86a
AlexNurzhanov <turk.alek@yandex.ru>
2022-09-08 19:41:21 +0400

    создал гитигнор и папку zad_041 (Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.)

---
+++ zad_041/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел происходит через Enter, останавливается при введении слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void NumberUser() {
+    string input = Console.ReadLine();
+    int count = 0;
+    while (input.ToLower() != "stop") {
+        if (int.Parse(input) > 0) count++;
+        input = Console.ReadLine();
+    }
+    Console.WriteLine(count);
+}
+
+NumberUser();
